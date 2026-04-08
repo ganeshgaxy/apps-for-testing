ffa9c79fcb205df97c908809eb28b3e13532bdb8
Ganeshan Manickam <ganeshan.manickam@showpad.com>
2026-04-08 17:22:05 +0200

    Skills update

--- apps/language-tail/src/App.tsx
+++ apps/language-tail/src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
+import { getSession } from "./auth";
+
+function RootRedirect() {
+  return <Navigate to={getSession() ? "/home" : "/login"} replace />;
+}
 
 export default function App() {
   return (
@@ -10,7 +15,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<RootRedirect />} />
       </Routes>
     </BrowserRouter>
   );
